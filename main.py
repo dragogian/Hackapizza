53487e50b0326cc27060d881861381a8b4b640ce
F69186D <g.dragonetti@reply.it>
2025-01-18 16:44:05 +0100

    fix file handling

--- main.py
+++ main.py
@@ -13,10 +13,14 @@ if __name__ == "__main__":
         for file in os.listdir("resources"):
             print(f"Loading file: {file}")
             load_file_documents_by_format(file, docs)
+            with open("docs_v3.pickle", "wb") as f:
+                pickle.dump(docs, f)
     else:
         print(f"Loading documents from pickle file...")
         docs = pickle.load(open("docs_v2.pickle", "rb"))
     print(f"Creating knowledge graph schema with loaded documents...")
     schema = create_knowledge_graph_schema(docs)
+    with open("schema.pickle", "wb") as f:
+        pickle.dump(schema, f)
     print(f"Creating knowledge graph with schema: \n {schema} \n")
     create_knowledge_graph(schema)
