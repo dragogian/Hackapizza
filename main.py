b760bf6f8176c75ad022ab9b05b34ce0e43421b7
F69186D <g.dragonetti@reply.it>
2025-01-18 14:58:56 +0100

    logs

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ load_dotenv()
 if __name__ == "__main__":
     docs = []
     for file in os.listdir("resources"):
+        print(f"Loading file: {file}")
         load_file_documents_by_format(file, docs)
-
+    print(f"Creating knowledge graph schema with loaded documents...")
     schema = create_knowledge_graph_schema(docs)
+    print(f"Creating knowledge graph with schema: \n {schema} \n")
     create_knowledge_graph(schema)
