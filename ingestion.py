be69502ef9519d5014c6378fa980089693a2921b
F69186D <g.dragonetti@reply.it>
2025-01-18 13:07:45 +0100

    ingestion loader

---
+++ ingestion.py
@@ -0,0 +1,20 @@
+from langchain_community.document_loaders import PyPDFLoader, CSVLoader, JSONLoader, TextLoader
+from langchain_core.documents import Document
+
+
+def load_file_documents_by_format(file: str, docs: list[Document]) -> list:
+    if file.endswith(".txt"):
+        loader = TextLoader(file)
+        docs.extend(loader.load_and_split())
+    elif file.endswith(".json"):
+        loader = JSONLoader(file, ".")
+        docs.extend(loader.load_and_split())
+    elif file.endswith(".csv"):
+        loader = CSVLoader(file)
+        docs.extend(loader.load_and_split())
+    elif file.endswith(".pdf"):
+        loader = PyPDFLoader(file, extract_images=True)
+        docs.extend(loader.load_and_split())
+    else:
+        raise ValueError("File format not supported")
+    return docs
